0e8551cd8c1e2ef28108e47c05f2d9ea9c6241fd
Jose Tovar <jose.jalisoft@gmail.com>
2023-03-02 22:44:55 -0500

    Format Currency Method

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
+import { formatCurrency, getCurrencySymbol } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ApiService } from '../http/http-api.service';
 
 @Component({
@@ -20,6 +21,17 @@ export class DashboardComponent implements OnInit {
   public updateProductForm: FormGroup = new FormGroup({});
 
   ngOnInit(): void {
+    // const myString: string = 'word.word.word';
+
+    // console.log(myString.replace('.', '$'));
+    // console.log(myString.replaceAll('.', '$'));
+
+    // const myNumber: string = '24343';
+    // console.log(myNumber);
+    // console.log(Number(myNumber));
+
+    // console.log(+myNumber);
+
     this.products$ = this.http.get<{
       id: number;
       name: string;
@@ -33,12 +45,41 @@ export class DashboardComponent implements OnInit {
         this.updateProductForm.addControl(
           `${product.id}`,
           new FormGroup({
-            price: new FormControl(product.price, Validators.min(0)),
+            price: new FormControl(formatCurrency(product.price, 'en-US', '$')),
             stock: new FormControl(product.stock, Validators.min(0)),
           })
         );
       });
     });
+  }
+
+  public setFormatCurrency(product: any, event: any): void {
+    const price = formatCurrency(
+      this.getValueFromCurrency(event.target.value),
+      'en-US',
+      '$'
+    );
+
+    this.updateProductForm.controls[product.id.toString()]
+      .get('price')
+      ?.setValue(price);
+  }
+
+  public getValueFromCurrency(value: string): number {
+    let price: number;
+
+    if (value.includes('$')) {
+      price = Number(value.substring(1).replaceAll(',', ''));
+    } else {
+      price = Number(value.replaceAll(',', ''));
+    }
+
+    // Methods for string
+    // 1. includes
+    // 2. substring()
+    // 3. replaceAll
+
+    return price;
   }
 
   public setUpdatedValues(product: any): void {
@@ -47,11 +88,13 @@ export class DashboardComponent implements OnInit {
       ...this.updateProductForm.controls[product.id].value,
     };
 
-    this.apiService
-      .updateSingeProduct(updatedValues)
-      .subscribe((response: any) => {
-        console.log(response);
-      });
+    console.log(updatedValues);
+
+    // this.apiService
+    //   .updateSingeProduct(updatedValues)
+    //   .subscribe((response: any) => {
+    //     console.log(response);
+    //   });
   }
 
   public setDisableValue(product: any): boolean {
